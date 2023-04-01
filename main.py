9fe6ee942d7d8e3049a78741933d3c02fe493f9d
Lalatendu07 <lalatendu.4391@gmail.com>
2023-04-01 18:41:02 +0530

    source code updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mushroom_clf.entity import config_entity
 from mushroom_clf.components.data_ingestion import DataIngestion 
 from mushroom_clf.components.data_validation import DataValidation
 from mushroom_clf.components.data_transformation import DataTransformation
+from mushroom_clf.components.model_trainer import ModelTrainer
 import os, sys
 
 if __name__ == "__main__":
@@ -24,7 +25,12 @@ if __name__ == "__main__":
           data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
           data_transformation =  DataTransformation(data_transformation_config=data_transformation_config,
                                                     data_ingestion_artifact=data_ingestion_artifact)   
-          data_transformation_artifact = data_transformation.initiate_data_transformation()                                                       
+          data_transformation_artifact = data_transformation.initiate_data_transformation()
+
+          #Model Trainer
+          model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+          model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+          model_trainer_artifact = model_trainer.initiate_model_trainer()                                                       
      except Exception as e :
           print(e)
 
