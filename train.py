3ddb0930854086d92402269cf144ab656e3b5ac4
Lalatendu07 <lalatendu.4391@gmail.com>
2023-04-11 15:56:55 +0530

    project complete

---
+++ train.py
@@ -0,0 +1,7 @@
+from mushroom_clf.pipeline.training_pipeline import start_training_pipeline
+
+if __name__ == "__main__":
+     try:
+         start_training_pipeline()
+     except Exception as e :
+          print(e)
